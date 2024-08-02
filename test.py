1e90f2df0e6109023218f998692528dba0862d47
kazjeane <jeanetrajano6@gmail.com>
2024-08-02 17:38:56 +0800

    First commit...

---
+++ test.py
@@ -0,0 +1,18 @@
+print('Hello World!')
+
+number_one = 10
+number_two = 13
+
+first_name = 'Kazyll'
+
+char_sample = 'c'
+
+people_age = {
+    'Kazyll': 20,
+    'Bj': 19,
+    'Mark': 30
+}
+
+total = number_one + number_two
+
+print ('The result is: ', total)
